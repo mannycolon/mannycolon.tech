3b3776ee652f424b6d6ed11cb70e9df00b7ce66c
mannycolon <colonmanuel7@gmail.com>
2018-05-25 18:39:19 -0400

    Added content to skills page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,7 +8,7 @@ const About = () => {
     <div className="about-page-container">
       <h1 className="title">About me</h1>
       <Avatar src="images/profile-pic-2.jpg" size={120} />
-      <p className="about-page-paragraph">
+      <p>
         My name is Manny. I'm a software engineer who designs, develops, 
         consults, speaks and writes about building high quality software
         applications. I live in New Jersey but I call many places home.
@@ -16,7 +16,7 @@ const About = () => {
         and can give never ending possibilities. Be thrilled to hear from 
         you regarding a software project or any other topic that may interest you.
       </p>
-      <p className="about-page-paragraph">
+      <p>
         I began my journey through computer science at the age of 15 by searching 
         for techie online tutorials. I then went to a technical high school where 
         I completed a technical education program in computer science.
