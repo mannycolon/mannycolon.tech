b879d4b62b99bb16017e876c0a4ce4a2de5e4284
mannycolon <colonmanuel7@gmail.com>
2018-08-12 18:27:13 -0400

    created page top intro part

---
+++ src/components/ProfileIntro/index.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from "classnames"
+// core components
+import GridContainer from "../../components/Grid/GridContainer"
+import GridItem from "../../components/Grid/GridItem"
+import Button from "../../components/CustomButtons/Button"
+
+import profile from "../../assets/img/profile-pic.jpg"
+
+const ProfileIntro = props => {
+  const { classes } = props;
+  const imageClasses = classNames(
+    classes.imgRaised,
+    classes.imgRoundedCircle,
+    classes.imgFluid
+  )
+  return (
+    <GridContainer justify="center">
+      <GridItem xs={12} sm={12} md={6}>
+        <div className={classes.profile}>
+          <div>
+            <img src={profile} alt="..." className={imageClasses} />
+          </div>
+          <div className={classes.name}>
+            <h2 className={classes.title}>Manny Colon</h2>
+            <h6>SOFTWARE ENGINEER</h6>
+            <Button justIcon
+                    link
+                    className={classes.margin5}
+                    href="https://www.instagram.com/_mannycolon"
+                    target="_blank">
+              <i className={"fab fa-instagram"} />
+            </Button>
+            <Button justIcon
+                    link
+                    className={classes.margin5}
+                    href="https://www.linkedin.com/in/mannycolon"
+                    target="_blank">
+              <i className={"fab fa-linkedin"} />
+            </Button>
+            <Button justIcon
+                    link
+                    className={classes.margin5}
+                    href="https://www.facebook.com/colonmanny"
+                    target="_blank">
+              <i className={"fab fa-facebook"} />
+            </Button>
+            <Button justIcon
+                    link
+                    className={classes.margin5}
+                    href="https://www.twitter.com/_mannycolon"
+                    target="_blank">
+              <i className={"fab fa-twitter"} />
+            </Button>
+            <Button justIcon
+                    link
+                    className={classes.margin5}
+                    href="https://www.github.com/mannycolon"
+                    target="_blank">
+              <i className={"fab fa-github"} />
+            </Button>
+          </div>
+        </div>
+      </GridItem>
+    </GridContainer>
+  );
+};
+
+ProfileIntro.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default ProfileIntro;
